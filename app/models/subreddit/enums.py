d64afb857ab996a4f7d770b3aa7c01666a68fd41
Fede Calendino <fede@calendino.com>
2022-08-12 19:41:59 +0200

    Add missing subreddit type

--- app/models/subreddit/enums.py
+++ app/models/subreddit/enums.py
@@ -5,6 +5,7 @@ class SubredditType(BaseEnum):
     ARCHIVED = "archived"
     BANNED = "banned"
     DELETED = "deleted"
+    EMPLOYEES_ONLY = "employees-only"
     GOLD_ONLY = "gold-only"
     PRIVATE = "private"
     PUBLIC = "public"
@@ -16,6 +17,7 @@ class SubredditType(BaseEnum):
 
 SUBREDDIT_TYPES = {
     "archived": SubredditType.ARCHIVED,
+    "employees_only": SubredditType.EMPLOYEES_ONLY,
     "gold_only": SubredditType.GOLD_ONLY,
     "private": SubredditType.PRIVATE,
     "public": SubredditType.PUBLIC,
