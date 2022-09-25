d75390151f79d3bf905e8853717e6bff4d067748
Fede Calendino <fede@calendino.com>
2022-09-25 18:21:32 +0200

    Beautify sql

--- release/sql/links.sql
+++ release/sql/links.sql
@@ -1,8 +1,14 @@
 SELECT 
-		links.source,
-		links.type,
-		links.target,
-		links.updated_at
-	FROM links LEFT JOIN subreddits ON links.target = subreddits.name
-		WHERE subreddits.type NOT IN ('non-existent', 'deleted')
-			ORDER BY links.source, links.type, links.target
+  links.source,
+  links.type,
+  links.target,
+  links.updated_at
+FROM
+  links
+  LEFT JOIN subreddits ON links.target = subreddits.name
+WHERE
+  subreddits.type NOT IN ('non-existent', 'deleted')
+ORDER BY
+  links.source,
+  links.type,
+  links.target
--- release/sql/subreddits_by_type.sql
+++ release/sql/subreddits_by_type.sql
@@ -1,19 +1,28 @@
 (
-	SELECT '**TOTAL**' AS type_, COUNT(*) AS amount
-		FROM subreddits
-			WHERE type NOT IN ('non-existent', 'deleted')
+  SELECT
+    '**TOTAL**' AS type_,
+    COUNT(*) AS amount
+  FROM
+    subreddits
+  WHERE
+    type NOT IN ('non-existent', 'deleted')
 )
 UNION
-(
-	SELECT 
-		CONCAT(
-			'**' || type || '**',
-			CASE WHEN nsfw THEN ' [nsfw]' ELSE '' END, 
-			CASE WHEN quarantined THEN ' [quarantined]' ELSE '' END
-		) AS type_,
-		COUNT(*) AS amount
-			FROM subreddits
-				WHERE type NOT IN ('non-existent', 'deleted')
-					GROUP BY type, nsfw, quarantined
-)
-ORDER BY amount DESC
+  (
+    SELECT
+      CONCAT(
+        '**' || type || '**', CASE WHEN nsfw THEN ' [nsfw]' ELSE '' END,
+        CASE WHEN quarantined THEN ' [quarantined]' ELSE '' END
+      ) AS type_,
+      COUNT(*) AS amount
+    FROM
+      subreddits
+    WHERE
+      type NOT IN ('non-existent', 'deleted')
+    GROUP BY
+      type,
+      nsfw,
+      quarantined
+  )
+ORDER BY
+  amount DESC
