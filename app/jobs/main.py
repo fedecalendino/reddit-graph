79053e6036df8a5b5279c12c447814b0263d3d5a
Fede Calendino <fede@calendino.com>
2023-02-14 11:10:54 +0100

    Update numbers

--- app/jobs/main.py
+++ app/jobs/main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import time
 
 from django.conf import settings
@@ -8,6 +9,11 @@ from app import actions
 from app.models.error import Error
 from app.models.queue import Queue
 from app.models.subreddit import Subreddit
+
+MAKE_RELEASE = int(os.getenv("MAKE_RELEASE", "250000"))
+ADD_POPULAR = int(os.getenv("POPULAR", "100000"))
+ADD_OUTDATED = int(os.getenv("ADD_OUTDATED", "2500"))
+ANALYZE_RANDOM = int(os.getenv("ANALYZE_RANDOM", "50"))
 
 logger = logging.getLogger(__name__)
 
@@ -20,16 +26,16 @@ def run():
         name = None
 
         try:
-            if counter % 50000 == 0:
+            if counter % MAKE_RELEASE == 0:
                 actions.make_release()
 
-            if counter % 10000 == 0:
+            if counter % ADD_POPULAR == 0:
                 actions.fill_queue_with_popular_subreddits()
 
-            if counter % 500 == 0:
+            if counter % ADD_OUTDATED == 0:
                 actions.fill_queue_with_outdated_subreddits()
 
-            if counter % 25 != 0:
+            if counter % ANALYZE_RANDOM != 0:
                 current = Queue.objects.order_by("-priority", "name").first()
             else:
                 current = None
